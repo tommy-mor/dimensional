b746d7aa02a8651fb29e7f8dbee3398a37d1860f
Tommy Morriss <thmorriss@gmail.com>
2017-11-20 21:10:01 -0800

    made text box persist and not break

--- dimensional-frontend/src/main/kotlin/main.kt
+++ dimensional-frontend/src/main/kotlin/main.kt
@@ -2,16 +2,14 @@ import kotlin.browser.*
 import kotlinx.html.*
 import kotlinx.html.dom.*
 import kotlinx.html.js.*
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import org.w3c.dom.HTMLElement
-import org.w3c.dom.Node
+import org.w3c.dom.*
 import org.w3c.dom.events.Event
 import org.w3c.dom.events.KeyboardEvent
 import org.w3c.dom.events.MouseEvent
 import kotlin.coroutines.experimental.suspendCoroutine
 import kotlin.dom.addClass
 import kotlin.dom.removeClass
+import kotlin.reflect.KProperty
 
 fun main(args: Array<String>) {
     window.onload = {
@@ -43,7 +41,7 @@ data class Table(val plates : List<Plate>) : Renderable {
     override fun wrap() : String = "latexthing ${plates.forEach { it.wrap() }}"
     override  fun render(parent : Element, document : Document) : HTMLElement  {
         val div = document.create.div("table")
-        plates.forEach { div.appendChild(it.render(parent, document)) }
+        plates.forEach { div.appendChild(it.render(div, document)) }
         return div
     }
 }
@@ -52,8 +50,8 @@ class Plate(val top : Segment, val bottom : Segment) : Renderable {
     override fun wrap() : String = "latex thing ${top.wrap()}, ${bottom.wrap()}"
     override fun render(parent: Element, document: Document) : HTMLElement {
         val div = document.create.div("plate") { + "plate" }
-        div.appendChild(top.render(parent, document))
-        div.appendChild(bottom.render(parent, document))
+        div.appendChild(top.render(div, document))
+        div.appendChild(bottom.render(div, document))
         /* div.addEventListener("mouseover", fun(event : Event) {
             event as MouseEvent
             div.addClass("red")
@@ -66,34 +64,46 @@ enum class State {
 }
 class Segment (var number : Double = 0.0, var unit : String = "cm", var element : String = "Na") : Renderable {
     var state : State = State.VIEW
-    constructor(input : String) : this () {
-        val all = input.split("")
+
+    fun updateOnString(input : String) {
+        val all = input.split(" ")
+        println(all)
         this.number = all[0].toDoubleOrNull() ?: throw IllegalArgumentException("not a double")
         this.unit = all[1]
         this.element = all[2]
-        //this class has html state because it needs to
+
     }
 
     override fun render(parent: Element, document: Document) : HTMLElement {
-        var a = document.create.div("segment") { + "${number} , ${unit} , and ${element}" }
-        a.addEventListener("click", { event->
+        var topDiv = document.create.div("segment")
+
+        var textDiv = document.create.div { id="displaybox"
+            +"$number $unit $element"}
+
+        topDiv.appendChild(textDiv)
+        topDiv.addEventListener("click", { event->
             if (state == State.VIEW) {
                 state = State.EDIT
-                a.appendChild(document.create.textArea {
-                    id = "editbox"
+
+                topDiv.removeChild(textDiv)
+                topDiv.appendChild(document.create.textArea { id = "editbox"
                     onKeyPressFunction = {event ->
-                    event as KeyboardEvent
-                    //enter key
-                    if (event.charCode == 0) {
-                        state = State.VIEW
-                        val elem = document.querySelector("#editbox")!!
-                        a.removeChild(elem)
+                        event as KeyboardEvent
+                        //is 0 on mac, 13 on linux. TODO test windows/find real solution
+                        if (event.charCode == 0 || event.charCode == 13) {
+                            val editbox = parent.querySelector("#editbox")!! as HTMLTextAreaElement
+                            updateOnString(editbox.value)
+                            state = State.VIEW
+                            topDiv.removeChild(editbox)
+                            textDiv.innerHTML = "$number $unit $element"
+                            topDiv.appendChild(textDiv)
+                        }
                     }
-                }
+                    + "$number $unit $element"
                 })
             }
         })
-        return a
+        return topDiv
     }
 
     override fun wrap() : String = "latext thing 1 $number $unit $element"
