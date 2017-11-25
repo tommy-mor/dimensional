275fad60cab366f21a43f1011f89dccc3293a21b
tommy-mor <thmorriss@gmail.com>
2017-11-24 21:07:14 -0500

    made a bunch of progress good job me

--- dimensional-frontend/src/main/kotlin/main.kt
+++ dimensional-frontend/src/main/kotlin/main.kt
@@ -19,6 +19,16 @@ import kotlin.reflect.KProperty
 //TODO work on wrap function to make into KaTex fomula
 //TODO FIX any bugs
 //TODO smooth out ui experience and make it look nice
+//TODO rewrite entire thing to make it not really trash. maybe find a kotlin ui frameowork - cont -
+// but DEF separate concerns more and make it recursive nice style like my interpreter
+//TODO auto cancelling units
+//TODO fix backwards tab, tab outside of textarea, tab inital thing is not number
+//TODO make site look nice, add explanation, be a hot designer and make it look boujee
+//TODO fix formatting for elements with subscripts
+//TODO fix "1" mode and fix the last elementa adding when ur tryna finish (add a better non-rendering plus thing)
+
+//mode codes - crossed, normal, one, equals
+//mode codes - x, n, 1, =
 
 var tableList = mutableListOf(Plate(Segment(),Segment()))
 var table = Table(tableList)
@@ -36,9 +46,8 @@ fun main(args: Array<String>) {
     window.onload = {
         document.body!!.append(table.render(document.body!!, document))
     }
-    val renderCode = table.wrap()
-    println(renderCode)
-    window.setInterval({katex.render(renderCode,renderDiv)},1000)
+    println(table.wrap())
+    window.setInterval({katex.render(table.wrap(),renderDiv)},1000)
 }
 
 fun fetch(count : String) {
@@ -62,7 +71,7 @@ data class Table(val plates : MutableList<Plate>) : Renderable {
         }
     }
 
-    override fun wrap() : String = "table (${plates.joinToString(", ") { it.wrap() }})"
+    override fun wrap() : String = "${plates.joinToString(" ") { it.wrap() }}"
     override  fun render(parent : Element, document : Document) : HTMLElement  {
         div.id = "tableId"
         plates.forEach { div.appendChild(it.render(div, document)) }
@@ -71,10 +80,20 @@ data class Table(val plates : MutableList<Plate>) : Renderable {
 }
 
 class Plate(val top : Segment, val bottom : Segment) : Renderable {
-    override fun wrap() : String = "Plate( ${top.wrap()}, ${bottom.wrap()})"
+
+    override fun wrap() : String  {
+        //TODO fix this egregious hack
+        var topString = top.wrap()
+        var prep = ""
+        if(top.mode == "=") {
+            topString = topString.drop(1)
+            prep = "="
+        }
+        return "$prep \\frac{$topString}{${bottom.wrap()}}"
+    }
 
     override fun render(parent: Element, document: Document) : HTMLElement {
-        val div = document.create.div("plate") { + "plate" }
+        val div = document.create.div("plate") { + "" }
         div.appendChild(top.render(div, document))
         div.appendChild(bottom.render(div, document))
         /* div.addEventListener("mouseover", fun(event : Event) {
@@ -88,21 +107,27 @@ class Plate(val top : Segment, val bottom : Segment) : Renderable {
         return top.state == State.NEW && bottom.state == State.NEW
     }
 }
+
+//these are not all mutually exclusive, <- code smell
 enum class State {
-    VIEW, EDIT, NEW
+    VIEW, EDIT, NEW, ONE, EQUALS
 }
 
 
-class Segment (var number : Double = 0.0, var unit : String = "Unit", var element : String = "Element") : Renderable {
+class Segment (var number : Double = 0.0, var unit : String = "Unit", var element : String = "Element", var mode : String="n") : Renderable {
     var state : State = State.NEW
-    var focusState = 2
+    var focusState = 0
 
     fun updateOnString(input : String) {
-        val all = input.split(",")
-        println(all)
-        this.number = all[0].toDoubleOrNull() ?: throw IllegalArgumentException("not a double")
-        this.unit = all[1]
-        this.element = all[2]
+        if (mode == "=") state = State.EQUALS
+        if (mode == "1") state = State.ONE; else {
+            val all = input.split(",")
+            println(all)
+            this.number = all[0].toDoubleOrNull() ?: throw IllegalArgumentException("not a double")
+            this.unit = all[1]
+            this.element = all[2]
+            this.mode = all[3].trim()
+        }
     }
 
     override fun render(parent: Element, document: Document) : HTMLElement {
@@ -134,25 +159,18 @@ class Segment (var number : Double = 0.0, var unit : String = "Unit", var elemen
                     onKeyDownFunction = {event ->
 
                         val editbox = parent.querySelector("#editbox")!! as HTMLTextAreaElement
-                        if(editbox.value.split(",").size < 3) editbox.value += ','
+                        if(editbox.value.split(",").size < 4) editbox.value += ','
                         event as KeyboardEvent
                         if (event.keyCode == 9) {
                             event.preventDefault()
                             val box = event.target as HTMLTextAreaElement
                             val a = box.value.split(",")
-                            if (focusState == 2) {
-                                focusState = 0
-                                box.selectionStart = 0
-                                box.selectionEnd = a[0].length
-                            } else if (focusState == 0) {
-                                focusState = 1
-                                box.selectionStart = a[0].length + 1
-                                box.selectionEnd = a[0].length + 1 + a[1].length
-                            } else {
-                                focusState = 2
-                                box.selectionStart = a[0].length + a[1].length + 2
-                                box.selectionEnd = a[0].length + a[1].length + a[2].length + 2
-                            }
+
+                            box.selectionStart = a.take(focusState).map { it.length }.sum() + focusState
+                            box.selectionEnd = a.take(focusState).map { it.length }.sum() + focusState + a[focusState].length
+
+                            focusState++
+                            focusState = focusState % 4
                         }
                     }
 
@@ -162,10 +180,10 @@ class Segment (var number : Double = 0.0, var unit : String = "Unit", var elemen
                         val editbox = parent.querySelector("#editbox")!! as HTMLTextAreaElement
 
                         if (event.charCode == 0 || event.charCode == 13) {
-                            updateOnString(editbox.value)
                             state = State.VIEW
+                            updateOnString(editbox.value)
                             topDiv.removeChild(editbox)
-                            textDiv.innerHTML = "$number $unit $element"
+                            textDiv.innerHTML = if(state == State.ONE) "-------" else "$number $unit $element"
                             topDiv.appendChild(textDiv)
                         }
                         if(event.keyCode == 9) {
@@ -174,7 +192,7 @@ class Segment (var number : Double = 0.0, var unit : String = "Unit", var elemen
                             this.selectionStart = 0
                         }
                     }
-                    + "$number, $unit, $element"
+                    + "$number, $unit, $element, $mode"
                 })
             }
         })
@@ -183,7 +201,19 @@ class Segment (var number : Double = 0.0, var unit : String = "Unit", var elemen
         return topDiv
     }
 
-    override fun wrap() : String = "Segment($number $unit $element)"
+    //TODO, element should not be wrap maybe cause it can't do subscripts, but non-text has bad font ehh
+    override fun wrap() : String {
+        if(state == State.ONE) {
+            return "1"
+        }
+        if (mode == "x") {
+            return "$number \\text{ \\ } \\cancel{$unit} \\text{\\  $element}"
+        } else if (mode == "="){
+            return "= $number \\text{ \\ } $unit \\text{\\  $element}"
+        } else{
+            return "$number \\text{ \\ } $unit \\text{\\  $element}"
+        }
+    }
 }
 
 
